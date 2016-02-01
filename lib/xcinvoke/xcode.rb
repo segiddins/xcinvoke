d182c04f1de0e3c8aa4769d8f017f05d79ee7b46
Samuel Giddins <segiddins@segiddins.me>
2016-02-01 17:27:43 -0600

    [Xcode] Dont use the custom env for computing the xcrun path

--- lib/xcinvoke/xcode.rb
+++ lib/xcinvoke/xcode.rb
@@ -74,7 +74,9 @@ module XCInvoke
 
     def xcrun(cmd, env: {}, err: false)
       env = env.merge(as_env)
-      @xcrun_path ||= Open3.capture2(env, 'xcrun', '-f', 'xcrun').first.strip
+
+      # explicitly dont use the env when computing the full path to xcrun
+      @xcrun_path ||= Open3.capture2('xcrun', '-f', 'xcrun').first.strip
       cmd = [@xcrun_path] + cmd
       case err
       when :merge
